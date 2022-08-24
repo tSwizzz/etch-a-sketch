1af438da01efa6e19edcf7490eb69947645201dc
Eddy G <eddy.spambari@gmail.com>
2022-08-24 00:33:58 -0700

    done, need to add non-disappearing hover effect

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-let validNum = false;
+const gridSize = document.getElementById("btnGridSize");
+    gridSize.addEventListener('click', changeGridSize);
+const gridColor = document.getElementById("btnGridColor");
+    gridColor.addEventListener('click', changeColor);
+
 let numOfGrids;
-do {
-numOfGrids = prompt("Select how many grids you want in your sketch pad between 2 and 100!");
-if(numOfGrids != null) {
-    if(numOfGrids < 2)          alert("Too Low!");
-    else if(numOfGrids > 100)   alert("Too High!");
-    else                        validNum = true;
-    }
-} while (!validNum);
-if(validNum) 
-    populate(numOfGrids);
+let validNum = false;
+function changeGridSize() {
+    do {
+        numOfGrids = prompt("Select how many grids you want in your sketch pad between 2 and 100!");
+        if(numOfGrids != null) {
+            if(numOfGrids < 2)          alert("Too Low!");
+            else if(numOfGrids > 100)   alert("Too High!");
+            else                        validNum = true;
+            }
+    } while (!validNum);
+    if(validNum)    populate(numOfGrids);
+}
+
+function changeColor() {
+    //WILL ADD ABILITY TO CHANGE PEN COLOR LATER, NOT REQUIRED FOR PROJECT
+}
+
+function oldColor() {
+
+}
+
+function newColor() {
+    square.style.backgroundColor = "blue";
+}
 
 function populate(size) {  
     let totalGrids = size * size;    
@@ -17,11 +35,12 @@ function populate(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
     for(let k = 0; k < totalGrids; k++) {
-        let grids = document.createElement("div");
+        grids = document.createElement("div");
         grids.classList.add("grids");
         container.appendChild(grids);
     }
 }
+
 
 
 
